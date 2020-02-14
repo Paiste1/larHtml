60335af18fcbf77a77b38a097663ab702164983c
Paiste1 <alexpaiste.ap@gmail.com>
2020-02-14 14:41:21 +0800

    hmtl

--- elements/head.php
+++
@@ -1,8 +0,0 @@
-﻿<title>Тест</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" type="text/css" href="/files/css/style.css">
-<link rel="stylesheet" type="text/css" href="/files/css/jquery.fancybox.min.css">
-<link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
-
-<link rel="icon" type="image/vnd.microsoft.icon" href="favicon.ico">
-<link rel="shortcut icon" href="favicon.ico">
--- elements/scripts.php
+++
@@ -1,4 +0,0 @@
-﻿<script type="text/javascript" src="/files/js/jquery.min.js"></script>
-<script type="text/javascript" src="/files/js/jquery.fancybox.min.js"></script>
-<script type="text/javascript" src="/files/js/scripts.js"></script>
-<script type="text/javascript" src="/files/js/form.js"></script>
